7965a25e9095b8cd08248b9a47b0278e718ed7d8
Diego Pereira <diego.junior10@gmail.com>
2016-12-01 23:50:02 -0200

    Inclusao inicial do swagger

--- api/src/main/java/br/com/cielo/resource/ExtratoResource.java
+++ api/src/main/java/br/com/cielo/resource/ExtratoResource.java
@@ -14,6 +14,7 @@ import br.com.cielo.resource.conversor.ExtratoConversor;
 import br.com.cielo.resource.json.ControleLancamentoJson;
 import br.com.cielo.resource.json.ExtratoJson;
 import br.com.cielo.service.ExtratoService;
+import io.swagger.annotations.ApiOperation;
 
 @RequestMapping("/extrato/v1")
 @RestController
@@ -21,19 +22,22 @@ public class ExtratoResource {
 
 	@Autowired
 	private ExtratoService service;
-	
+
 	@Autowired
 	private ExtratoConversor conversor;
-	
+
 	@RequestMapping("/lancamentos/{indice}/{tamanhoPagina}/{totalElementos}")
-	public ResponseEntity<List<ControleLancamentoJson>> lancamentos(@PathVariable(value = "indice") final String indiceParam,
+	@ApiOperation(value = "lancamentos", nickname = "Lancamentos")
+	public ResponseEntity<List<ControleLancamentoJson>> lancamentos(
+			@PathVariable(value = "indice") final String indiceParam,
 			@PathVariable(value = "tamanhoPagina") final String tamanhoPaginaParam,
 			@PathVariable(value = "totalElementos") final String totalElementosParam) {
-		
+
 		Extrato extrato = this.service.gerar();
 		ExtratoJson extratoJson = conversor.converterFrom(extrato);
-		
-		return new ResponseEntity<List<ControleLancamentoJson>>(extratoJson.getListaControleLancamento(), HttpStatus.OK);
+
+		return new ResponseEntity<List<ControleLancamentoJson>>(extratoJson.getListaControleLancamento(),
+				HttpStatus.OK);
 	}
-	
+
 }
